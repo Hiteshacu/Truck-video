f49c08f8931779fcd1bb378f77b7c26758685853
Hiteshacu <hiteshacu@gmail.com>
2025-04-14 09:13:48 +0530

    extra

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+
 const userSchema = new mongoose.Schema({
     fullname: {
         firstname: {
@@ -29,6 +30,7 @@ const userSchema = new mongoose.Schema({
         type: String,
     },
 })
+
 userSchema.methods.generateAuthToken = function () {
     const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
     return token;
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { body } = require("express-validator");
+const { body } = require("express-validator")
 const userController = require('../controllers/user.controller');
-const authMiddleware = require('../middlewares/auth.middleware'); 
+const authMiddleware = require('../middlewares/auth.middleware');
+
 
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
@@ -19,8 +20,9 @@ router.post('/login', [
     userController.loginUser
 )
 
-router.get('/profile',authMiddleware.authUser, userController.getUserProfile)
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
 
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 
 
