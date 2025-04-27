932f5f8028b644214214d69049ee2a422c941b9d
Hiteshacu <hiteshacu@gmail.com>
2025-04-27 17:40:34 +0530

    More

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='h-screen w-full'>
+        <div>
+            <hr>Get started with Travo</hr>
+            <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
